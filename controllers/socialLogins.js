894c4cb4d4c65fe26479f9ca12dbd26c78e29c0f
Sammymarigamukami <marigasam18@gmailcom>
2025-12-31 18:23:06 +0300

    first commit

--- controllers/socialLogins.js
+++ controllers/socialLogins.js
@@ -140,7 +140,7 @@ export const githubCallback = async (req, res) => {
         // Set access token cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
+            secure: process.env.NODE_ENV === "production",
             sameSite: "lax",
             maxAge: 15 * 60 * 1000 // 15 minutes
         });
@@ -148,7 +148,7 @@ export const githubCallback = async (req, res) => {
         // Set refresh token cookie
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: true,
+            secure: process.env.NODE_ENV === "production",
             sameSite: "lax",
             path: "/auth/refresh-token",   // restrict refresh token cookie to only be sent to this endpoint
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
